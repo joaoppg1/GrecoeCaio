311125e04a8e536de2074e8d932c96dcad2ed88d
joaoppg1 <joaoppg1@al.insper.edu.br>
2026-04-30 21:12:43 -0300

    Exercício 2

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,4 @@
+#ex 1
 import random
 
 def rolar_dados(qtd):
@@ -8,4 +9,17 @@ def rolar_dados(qtd):
         lista.append(dado)
     
     return lista
+    
+#ex 2
 
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+    valor = dados_rolados[dado_para_guardar]
+    
+    nova_rolados = []
+    for i in range(len(dados_rolados)):
+        if i != dado_para_guardar:
+            nova_rolados.append(dados_rolados[i])
+    
+    novo_estoque = dados_no_estoque + [valor]
+    
+    return [nova_rolados, novo_estoque]
